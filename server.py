13be9ab7c13d9254c837fe13947deb82985abb92
Vivek Narayanan <vivek_n@me.com>
2013-10-07 22:10:08 +0530

    Add styles, ajax

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, jsonify
 from info import classify2
 from math import e
+from redis import Redis
+from config import STATS_KEY, HOST, RHOST, RPASS, RPORT
 
 app = Flask(__name__)
 app.debug = True
+conn = Redis(RHOST, RPORT, password=RPASS)
 
 def percentage_confidence(conf):
 	return 100.0 * e ** conf / (1 + e**conf)
@@ -19,6 +22,7 @@ def get_sentiment_info(text):
 
 @app.route('/')
 def home():
+	conn.incr(STATS_KEY + "_hits")
 	return render_template("index.html")
 
 @app.route('/api/text/', methods=["POST"])
@@ -26,15 +30,17 @@ def read_api():
 	text = request.form.get("txt")
 	sentiment, confidence = get_sentiment_info(text)
 	result = {"sentiment": sentiment, "confidence": confidence}
+	conn.incr(STATS_KEY + "_api_calls")
 	return jsonify(result=result)
 
 @app.route('/web/text/', methods=["POST"])
 def evaldata():
 	text = request.form.get("txt")
 	result, confidence = get_sentiment_info(text)
-	return render_template('result.html', result=result, confidence=confidence, sentence=text)
+	conn.incr(STATS_KEY + "_web_calls")
+	return jsonify(result=result, confidence=confidence, sentence=text)
 
 @app.route('/docs/api/')
 def api():
-	return render_template('api.html')
+	return render_template('api.html', HOST)
 	
