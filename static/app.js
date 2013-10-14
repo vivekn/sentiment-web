00dd9c887c0d14f14d5152e958fd1ac345b58555
Vivek Narayanan <vivek_n@me.com>
2013-10-14 19:41:32 +0530

    Fix form encoding issue

--- static/app.js
+++ static/app.js
@@ -1,19 +1,8 @@
-function xhrPost(url, params, callback) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, true);
-	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-	xhr.onload = callback;
-	xhr.send(params);
-}
-
 var btn = document.getElementById('submit');
 var tpl = _.template(document.getElementById('templ-results').innerHTML);
 btn.addEventListener("click", function(event) {
 	var txt = document.getElementById('txt').value;
-	console.log(txt);
-	var params = "txt="+txt;
-	xhrPost('web/text/', params, function () {		
-		var data = JSON.parse(this.responseText);
+	$.post('web/text/', {"txt": txt}, function (data) {		
 		document.getElementById('result').innerHTML = tpl(data);
 	});
 });
